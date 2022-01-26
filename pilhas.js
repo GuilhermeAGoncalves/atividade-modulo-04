d1e472268f61467b73b2d928b46e2b80b27dc7f6
GuilhermeAGoncalves <guilhermegpop@gmail.com>
2022-01-25 21:36:16 -0300

    pequenas modificacoes

--- pilhas.js
+++ pilhas.js
@@ -43,7 +43,44 @@ const pilha = new createStack();
 
 pilha.add(4, 1,2,3,4);
 pilha.remove(2);
-pilha.add(4, 1,2,3,4)
-pilha.add(10, 1,2,3,4,6,7,8,9,10,11)
+pilha.add(4, 1,2,3,4);
+pilha.add(10, 1,2,3,4,6,7,8,9,10,11);
 console.log(pilha.print())
-pilha.remove(10) 
+pilha.remove(10)
+
+/*
+1 adicionado com sucesso
+2 adicionado com sucesso
+3 adicionado com sucesso
+4 adicionado com sucesso
+4 removido com sucesso
+3 removido com sucesso
+1 ja está contido na pilha
+2 ja está contido na pilha
+3 adicionado com sucesso        ----
+1 ja está contido na pilha      ----
+4 adicionado com sucesso        ----
+6 adicionado com sucesso        ----
+7 adicionado com sucesso        ----
+2 ja está contido na pilha      ----
+3 ja está contido na pilha      ----
+4 ja está contido na pilha      ----
+8 adicionado com sucesso        ----
+9 adicionado com sucesso        
+10 adicionado com sucesso       
+11 adicionado com sucesso      
+[
+  1, 2, 3,  4,  6,
+  7, 8, 9, 10, 11
+]
+11 removido com sucesso
+10 removido com sucesso
+9 removido com sucesso
+8 removido com sucesso
+7 removido com sucesso
+6 removido com sucesso
+4 removido com sucesso
+3 removido com sucesso
+2 removido com sucesso
+1 removido com sucesso
+*/
